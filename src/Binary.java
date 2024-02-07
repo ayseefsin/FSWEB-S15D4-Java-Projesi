3eff910ca2e348f47ef6b1687ae45bb8efec5d85
ayseefsin <ayse.efsin@yandex.com>
2024-02-07 20:28:40 +0300

    decimal to binary done

--- src/Binary.java
+++ src/Binary.java
@@ -1,8 +1,22 @@
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
 public class Binary {
 
+    public static void main(String[] args){
+        System.out.println(decimalToBinary(6));
+    }
 
+    public static LinkedList<Integer> decimalToBinary(int decimal){
+        LinkedList<Integer> binary = new LinkedList<>();
+        while(decimal>0){
+            int remainder = decimal%2;
+            binary.push(remainder);
+            decimal /=2;
+        }
+        System.out.println("binary: " + binary);
+        return binary;
+    }
 
 }
